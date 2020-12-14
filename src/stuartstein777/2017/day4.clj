dd5ef6d5141b5ec298e52eda0cdf471b859caeb7
Stuart Stein <qmstuart@gmail.com>
2020-12-14 02:50:18 +0000

    Create day4.clj

---
+++ src/stuartstein777/2017/day4.clj
@@ -0,0 +1,31 @@
+(ns stuarts.2017day4
+  (:require [clojure.string :as str]))
+
+;; part 1
+(defn duplicate-words? [line]
+  (->> (frequencies line)
+       (vals)
+       (filter (fn [wc] (> wc 1)))))
+
+#_(duplicate-words? ["jsdmfw" "ptjwrbl" "hhuv" "uolz" "adyweh" "qpj" "wxyogp" "igvnojq" "jmfw" "pqs" "fsnirby"])
+
+(defn parse-input []
+  (->> (slurp "puzzle-inputs/2017/day4")
+       (str/split-lines)
+       (map #(str/split % #" "))))
+
+(let [input (parse-input)]
+  (->> (map duplicate-words? input)
+       (filter #(not (seq %)))
+       (count)))
+
+;; part 2
+(defn valid? [passphrase]
+  (let [word-sets (map set passphrase)
+        all-count (count word-sets)
+        distinct-count (count (distinct word-sets))]
+    (zero? (- all-count distinct-count))))
+
+(let [input (parse-input)]
+  (->> (filter valid? input)
+       (count)))
