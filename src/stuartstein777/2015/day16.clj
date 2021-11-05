88f6d4ac6a4993730057ff93f171b79fed39da99
Stuart Stein <qmstuart@gmail.com>
2021-11-05 21:02:27 +0000

    Create day16.clj

---
+++ src/stuartstein777/2015/day16.clj
@@ -0,0 +1,51 @@
+(defn parse-aunt [aunt-line]
+  (let [num-idx (.indexOf aunt-line ":")
+        num (Integer/parseInt (subs aunt-line 4 num-idx))
+        stats (subs aunt-line (+ 2 num-idx))]
+    (->> (str/split stats #", ")
+         (map #(str/split % #": "))
+         (map (fn [[k v]] {(keyword k) (Integer/parseInt v)}))
+         (into {:num num}))))
+
+(defn matches [f v k a]
+  (or (nil? (k a)) (f v (k a))))
+
+;; part 1
+
+(let [all-aunts (->> (slurp "inputs/2015-16")
+                     (str/split-lines)
+                     (mapv parse-aunt))]  
+  (-> (filter (every-pred
+                (partial matches = 3 :children)
+                (partial matches = 7 :cats)
+                (partial matches = 2 :samoyeds)
+                (partial matches = 3 :pomeranians)
+                (partial matches = 0 :akitas)
+                (partial matches = 0 :vizslas)
+                (partial matches = 5 :goldfish)
+                (partial matches = 3 :trees)
+                (partial matches = 2 :cars)
+                (partial matches = 1 :perfumes))
+               all-aunts)
+      first
+      :num))
+
+;; part 2
+
+(let [all-aunts (->> (slurp "inputs/2015-16")
+                     (str/split-lines)
+                     (mapv parse-aunt))]  
+  (-> (filter (every-pred
+                (partial matches = 3 :children)
+                (partial matches < 7 :cats)
+                (partial matches = 2 :samoyeds)
+                (partial matches > 3 :pomeranians)
+                (partial matches = 0 :akitas)
+                (partial matches = 0 :vizslas)
+                (partial matches > 5 :goldfish)
+                (partial matches < 3 :trees)
+                (partial matches = 2 :cars)
+                (partial matches = 1 :perfumes))
+               all-aunts)
+      first
+      :num))
