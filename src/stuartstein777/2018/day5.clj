7097924cb0f17d934b5033398fc34a5b1e52c41f
Stuart Stein <qmstuart@gmail.com>
2021-11-06 16:21:34 +0000

    day 5 refactor

--- src/stuartstein777/2018/day5.clj
+++ src/stuartstein777/2018/day5.clj
@@ -1,13 +1,14 @@
 (ns stuartstein777.2018.day5
   (:require [clojure.string :as str]))
 
-(def reactors (mapcat (fn [s] [(str (str/upper-case s) s) (str s (str/upper-case s))]) "abcdefghijklmnopqrstuvwxyz"))
+(defn make-reactor [s]
+  (let [reactor (str (str/upper-case s) s)]
+    [reactor (str/reverse reactor)]))
 
-(defn replacer [s m]
-  (str/replace s m ""))
+(def reactors (mapcat make-reactor "abcdefghijklmnopqrstuvwxyz"))
 
 (defn react [s]
-  (let [reacted (reduce (fn [acc m] (replacer acc m)) s reactors)]
+  (let [reacted (reduce (fn [acc m] (str/replace acc m "")) s reactors)]
     (if (= s reacted)
       reacted
       (recur reacted))))
@@ -18,12 +19,11 @@
     (count))
 
 ;; part 2
+(defn reactor-pattern [s]
+  (re-pattern (str (subs s 0 1) "|" (subs s 1))))
+
 (let [orig (slurp "puzzle-inputs/2018/day5")
-      all (map (fn [s]
-                 (-> orig
-                     (str/replace (subs s 0 1) "")
-                     (str/replace (subs s 1) ""))) (take-nth 2 reactors))]
+      all (map #(str/replace orig (reactor-pattern %) "") (take-nth 2 reactors))]
   (->> (map react all)
        (map count)
        (apply min)))
-
