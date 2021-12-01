4be1ad9332f96738de2bfc3fe12c34cb2052280c
Stuart Stein <qmstuart@gmail.com>
2021-12-01 09:55:33 +0000

    refactoring 2021 day 1

--- src/stuartstein777/2021/day1.clj
+++ src/stuartstein777/2021/day1.clj
@@ -1,19 +1,22 @@
 (ns stuartstein777.2021.day1
-  (:require [clojure.string :as str]
-            [clojure.set :as set]
-            [stuartstein777.file :as f]))
+  (:require [stuartstein777.file :as f]))
+
+(def puzzle-input "puzzle-inputs/2021/day1")
 
 (defn calculate-increasing [xs]
   (->> (map < xs (rest xs))
        (filter true?)
        (count)))
 
+(defn parser [n]
+  (Integer/parseInt n))
+
 ;; Part 1
-(->> (f/read-all-lines-and-parse "puzzle-inputs/2021/day1" (fn [n] (Integer/parseInt n)))
+(->> (f/read-all-lines-and-parse puzzle-input parser)
      calculate-increasing)
 
 ;; Part 2
-(->> (f/read-all-lines-and-parse "puzzle-inputs/2021/day1" (fn [n] (Integer/parseInt n)))
+(->> (f/read-all-lines-and-parse puzzle-input parser)
      (partition 3 1)
      (map #(reduce + %))
      calculate-increasing)
--- src/stuartstein777/2021/day2.clj
+++ src/stuartstein777/2021/day2.clj
@@ -3,6 +3,8 @@
             [clojure.set :as set]
             [stuartstein777.file :as file]))
 
+(def puzzle-input "puzzle-inputs/2021/day2")
+
 ; part 1
 
 ; part 2
