e70358f1583147b6cf7df05c096cbe91a390ccda
Stuart Stein <qmstuart@gmail.com>
2021-12-07 20:02:39 +0000

    Update file.clj

--- src/stuartstein777/utils/file.clj
+++ src/stuartstein777/utils/file.clj
@@ -1,5 +1,6 @@
 (ns stuartstein777.file
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str]
+            [stuartstein777.utils :as u]))
 
 (defn read-all-lines [file]
   (->> (slurp file)
@@ -10,3 +11,9 @@
   (->> file
        (read-all-lines)
        (map parsefn)))
+
+(defn parse-csv-ints [file]
+  (->> file
+       (slurp)
+       (u/str-split #",")
+       (map #(Integer/parseInt %))))
