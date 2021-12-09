ef7c1336821003b4b18603fc3685a92195c2305b
Stuart Stein <qmstuart@gmail.com>
2021-12-09 22:00:48 +0000

    Update day9.clj

--- src/stuartstein777/2021/day9.clj
+++ src/stuartstein777/2021/day9.clj
@@ -20,35 +20,22 @@
                 l (get-in input [h (dec w)] 10)
                 r (get-in input [h (inc w)] 10)]
           :when (and (< n u) (< n d) (< n l) (< n r))]
-      n)))
+      {[h w] n})))
 
 ;; part 1
 (let [input (->> (f/read-all-lines-and-parse "puzzle-inputs/2021/day9" parser))]
   (->> input
        (find-low-points)
+       (mapcat vals)
        (map inc)
-       (reduce +)))
+       (reduce +))) ; 436
 
-
+#_(str \a \u0333)
 
 ;; part 2
 ;; do this with a stack ? Store locations and recur till stack is empty, as we recur keep count of locations.
 ;; take the top 3 and find their product.
 ;; need to keep track of already visited.
-
-(defn find-low-points [input]
-  (let [height (count input)
-        width (count (first input))]
-    (for [w (range 0 width)
-          h (range 0 height)
-          :let [n (get-in input [h w])
-                u (get-in input [(dec h) w] 10)
-                d (get-in input [(inc h) w] 10)
-                l (get-in input [h (dec w)] 10)
-                r (get-in input [h (inc w)] 10)]
-          :when (and (< n u) (< n d) (< n l) (< n r))]
-      [h w])))
-
 (defn get-neighbours [input [y x]]
   (let [yx' [[y (dec x)] [y (inc x)] [(dec y) x] [(inc y) x]]]
     (remove (fn [yx] (= 9 (get-in input yx 9))) yx')))
@@ -59,14 +46,15 @@
     (let [yx         (first to-visit)
           neighbours (->> (get-neighbours input yx)
                           (remove (fn [yx] (visited yx))))]
-      (if (visited yx)
-        (recur input visited size (rest to-visit))
-        (recur input (conj visited yx) (inc size) (distinct (into (rest to-visit) neighbours)))))))
+      (recur input (conj visited yx) (inc size) (distinct (into (rest to-visit) neighbours))))))
 
-(let [input (->> (f/read-all-lines-and-parse "puzzle-inputs/2021/day9" parser))
-      low-points (find-low-points input)]
-  (->> (map vector low-points)
-       (map (partial walk-basins input #{} 0))
+(let [input (f/read-all-lines-and-parse "puzzle-inputs/2021/day9" parser)
+      walk-basin-fn (partial walk-basins input #{} 0)]
+  (->> input
+       (find-low-points)
+       (mapcat keys)
+       (map vector)
+       (map walk-basin-fn)
        (sort >)
        (take 3)
        (reduce * 1)))
