fda4de8ba1ee3457e59547efe531d4cbd4de74c5
Stuart Stein <qmstuart@gmail.com>
2022-12-07 09:38:51 +0000

    WIP: 2022 day 7

--- src/stuartstein777/2022/day7.clj
+++ src/stuartstein777/2022/day7.clj
@@ -23,21 +23,19 @@
     [(mapv get-dir-name dirs)
      (mapv get-file-info files)]))
 
-(defn update-file-structure [[files dirs] current-director file-system]
-    
-    )
+(defn update-file-structure [[dirs files] dir fs]
+    (assoc fs dir {:files (set files) :dirs (set dirs)}))
 
 (defn get-listings [rcmds]
   (->> rcmds
       (take-while #(not (str/starts-with? % "$")))))
 
 (defn process-commands [stack file-structure [cmd & rcmds]]
-  #_#_(tap> (str "cmd: " cmd))
-  (tap> stack)
+  (prn stack)
   (if  (seq cmd)
     (cond
       (str/starts-with? cmd "$ cd ..")
-      (recur (rest stack) file-structure rcmds)
+      (recur (vec (butlast stack)) file-structure rcmds)
 
       (str/starts-with? cmd "$ cd /")
       (recur ["/"] file-structure rcmds)
@@ -50,54 +48,36 @@
       (str/starts-with? cmd "$ ls")
       (let [listings (get-listings rcmds)
             files-dirs (process-listings listings)
-            updated-file-structure (update-file-structure files-dirs (last stack))]
-        (prn files-dirs)
-        (tap> files-dirs)
-        (recur stack file-structure (drop (count listings) rcmds)))
+            updated-file-structure
+            (update-file-structure files-dirs (last stack) file-structure)]
+        (recur stack updated-file-structure (drop (count listings) rcmds)))
       
       :else
       (recur stack file-structure rcmds))
     file-structure))
 
-               
 (->> (slurp "puzzle-inputs/2022/day7-test")
      (str/split-lines)
      (process-commands ["/"] {}))
 
 ;; outer dirctory = /
-["$ cd /"
- "$ ls"
- "dir a"
- "14848514 b.txt"
- "8504156 c.dat"
- "dir d"
- "$ cd a"
- "$ ls"
- "dir e"
- "29116 f"
- "2557 g"
- "62596 h.lst"
- "$ cd e"
- "$ ls"
- "584 i"
- "$ cd .."
- "$ cd .."
- "$ cd d"
- "$ ls"
- "4060174 j"
- "8033020 d.log"
- "5626152 d.ext"
- "7214296 k"]
+
 
 ;; keys are directories
-{"/" {:dirs #{"a"} :files #{{:name "b.txt" :size 14848514}
+{"/" {:dirs #{"a" "d"} :files #{{:name "b.txt" :size 14848514}
                             {:name "c.dat" :size 8504156}}}
+ 
  "a" {:dirs #{"e"} :files #{{:name "f" :size 29116}
                             {:name "g" :size 2557}
                             {:name "h.lst" :size 62596}}}
+ 
  "d" {:dirs #{} :files #{{:name "j" :size 4060174}
                        {:name "d.log" :size 8033020}
                        {:name "d.ext" :size 5626152}
                        {:name "k" :size 721496}}}
+ 
  "e" {:dirs #{} :files #{{:name "i" :size 584}}}
  }
+
+
+
