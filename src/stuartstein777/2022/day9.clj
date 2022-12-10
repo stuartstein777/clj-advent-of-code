3a31631addf40b723e151bbd2223d42bff3bd0cb
Stuart Stein <qmstuart@gmail.com>
2022-12-10 01:34:55 +0000

    refactoring 2022 day 9 part 1

--- src/stuartstein777/2022/day9.clj
+++ src/stuartstein777/2022/day9.clj
@@ -3,7 +3,7 @@
             [clojure.string :as str]
             [medley.core :as med]))
 
-(defn get-neighbours [[x y]]
+(defn neighbours [[x y]]
   (set 
    (for [Δx [-1 0 1]
          Δy [-1 0 1]
@@ -12,66 +12,43 @@
      neighbour)))
 
 (defn tail-needs-to-move? [hxy txy]
-  (not ((get-neighbours hxy) txy)))
+  (not ((neighbours hxy) txy)))
 
 (defn move-tail [[hx hy] [tx ty]]
   (cond
-    (and (= hx tx) (= hy ty))
+    
+    ;; tail and head are same position
+    (and (= hx tx) (= hy ty)) 
     [tx ty]
     
-    (= hx tx) ;; if tail is on same x as head
+    ;; if tail is on same x as head
+    (= hx tx) 
     [hx (if (> hy ty) (dec hy) (inc hy))]
-    (= hy ty) ;; if tail is on same y as head
+    
+    ;; if tail is on same y as head
+    (= hy ty) 
     [(if (> hx tx) (dec hx) (inc hx)) hy]
 
     ;; diagonals
-    (and (< hx tx) (= 2 (- ty hy)))
+    (or (and (< hx tx) (= 2 (- ty hy)))
+        (and (> hx tx) (= 2 (- ty hy))))
     [hx (inc hy)]
 
-    (and (< hx tx) (= 1 (- ty hy)))
+    (or (and (< hx tx) (= 1 (- ty hy)))
+        (and (< hx tx) (= 1 (- hy ty))))
     [(inc hx) hy]
 
-    (and (> hx tx) (= 2 (- ty hy)))
-    [hx (inc hy)]
-
-    (and (> hx tx) (= 1 (- ty hy)))
+    (or (and (> hx tx) (= 1 (- ty hy)))
+        (and (> hx tx) (= 1 (- hy ty))))
     [(dec hx) hy]
 
-    (and (> hx tx) (= 2 (- hy ty)))
-    [hx (dec hy)]
+    (or (and (> hx tx) (= 2 (- hy ty)))
+        (and (< hx tx) (= 2 (- hy ty))))
+    [hx (dec hy)]))
 
-    (and (> hx tx) (= 1 (- hy ty)))
-    [(dec hx) hy]
-
-    (and (< hx tx) (= 1 (- hy ty)))
-    [(inc hx) hy]
-    
-    (and (< hx tx) (= 2 (- hy ty)))
-    [hx (dec hy)]
-    
-    :else
-    (do (prn "argh!" [hx hy] [tx ty])
-        (throw "wtf"))
-    ))
-;; combine some of these based on returned value.
-
-(= [-4 3] (move-tail [-4 2] [-3 4]))
-(= [-3 2] (move-tail [-4 2] [-2 3]))
-(= [-2 3] (move-tail [-2 2] [-3 4])) 
-(= [-3 2] (move-tail [-2 2] [-4 3]))
-(= [-2 3] (move-tail [-2 4] [-3 2]))
-(= [-3 4] (move-tail [-2 4] [-4 3])) 
-(= [-3 4] (move-tail [-4 4] [-2 3]))
-(= [-4 3] (move-tail [-4 4] [-3 2]))
-
-;; > hy ty
-;; < hx tx
-;; [hx (inc hx)]
-;;          hx hy  tx ty
-(move-tail [-4 2] [-3 4]) ; [-4 3]
 
 (defn parse-input []
-  (->> (slurp "puzzle-inputs/2022/day9-test")
+  (->> (slurp "puzzle-inputs/2022/day9")
        (str/split-lines)
        (map (fn [l] (let [[a b] (str/split l #"\s")]
                       [a (parse-long b)])))))
@@ -92,7 +69,6 @@
                        (move-tail new-head-pos tail)
                        tail)
         new-visited (conj tail-visited new-tail-pos)]
-    (prn new-head-pos "::" new-tail-pos)
     {:head new-head-pos
      :tail new-tail-pos
      :tail-visited new-visited}))
@@ -104,54 +80,4 @@
       :tail-visited
       count)
 
-;; 6808 too high
-
-;; test answer should be 13
-
-(comment
-  
-  
-     *
-  [[-4 2] [-4 3] [-4 4]
-   [-3 2] [-3 3] [-3 4] *
-   [-2 2] [-2 3] [-2 4]]
-
-;; hy > ty
-;; hx < tx
-;; [hx (inc hy)]
-  '[[H] [ ] [ ]
-    [ ] [ ] [T]
-    [ ] [ ] [ ]]
-
-  
-;; hy < ty
-;; hx < tx
-;; [hx (dec hy)]
-  '[[H] [ ] [ ] 
-    [.] [ ] []
-    [ ] [T] [ ]]
-  
-
-;; hy < ty
-;; hx > tx
-;; [hx (dec hy)]
-  '[[ ] [ ] [H]
-    [ ] [ ] [.]
-    [ ] [T] [ ]]
-  
-
-;; hy > ty
-;; hx > tx
-;; [hx (inc hy)]
-  '[[ ] [T] [ ]
-    [ ] [ ] [.]
-    [ ] [ ] [H]]
-  
-
-;; hy > ty
-;; hx > tx
-;; [hx (inc hy)]
-  '[[ ] [T] [ ]
-    [.] [ ] [ ]
-    [H] [ ] [ ]]
-  )
+;; 6503
