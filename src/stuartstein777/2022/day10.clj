7093bf7963ffd0e23d79041ae4d93196f626545f
Stuart Stein <qmstuart@gmail.com>
2022-12-12 20:50:00 +0000

    Solved Day 10 Part 2

--- src/stuartstein777/2022/day10.clj
+++ src/stuartstein777/2022/day10.clj
@@ -1,7 +1,7 @@
-(ns stuartstein777.2022.day10
-  (:require [clojure.set :as set]
-            [clojure.string :as str]
-            [medley.core :as med]))
+(ns exfn.messing
+  (:require [clojure.string :as str]
+            [clojure.set :as set]
+            [clojure.math.combinatorics :as combo]))
 
 (defn parse-line [l]
   (let [[inst v] (str/split l #"\s")]
@@ -13,7 +13,7 @@
 
 (defn process [input cycles-to-log]
   (reduce 
-   (fn [{:keys [cycle x cycles-log] :as acc}
+   (fn [{:keys [cycle x cycles-log x-log] :as acc}
        {:keys [inst v] :as i}] 
      (let [new-cycle (inc cycle)
            new-x (if (= inst :noop)
@@ -22,12 +22,14 @@
            new-cycles-log (if (cycles-to-log new-cycle)
                             (conj cycles-log (* new-cycle new-x))
                             cycles-log)]
-       (prn cycle new-x)
+       #_(prn cycle new-x)
             {:cycle new-cycle
              :x new-x
-             :cycles-log new-cycles-log}))
-          {:cycle 1 :x 1 :cycles-log []} input))
+             :cycles-log new-cycles-log
+             :x-log (conj x-log x)}))
+          {:cycle 1 :x 1 :cycles-log [] :x-log []} input))
 
+;; part 1
 (let [input (->> (slurp "puzzle-inputs/day10")
                  (str/split-lines)
                  (mapcat parse-line))
@@ -35,3 +37,32 @@
   (->> (process input cycles-to-log)
        :cycles-log
        (reduce +)))
+
+
+;; part 2
+(let [input (->> (slurp "puzzle-inputs/day10")
+                 (str/split-lines)
+                 (mapcat parse-line))
+      cycles-to-log (set (take 11 cycles-to-log))
+      x-log (->> (process input cycles-to-log)
+                 :x-log)]
+  (println
+   (->>
+    (reduce 
+     (fn [acc i]
+       (if (<= (Math/abs (- (mod i 40) (nth x-log i))) 1)
+         (str acc "⭐")
+         (str acc "⬛")))
+     "" (range 0 240))
+    (partition 40)
+    (map (partial apply str))
+    (str/join "\n"))))
+
+"
+⭐⭐⭐⬛⬛⭐⭐⭐⭐⬛⭐⬛⬛⭐⬛⭐⭐⭐⭐⬛⬛⭐⭐⬛⬛⭐⭐⭐⬛⬛⭐⭐⭐⭐⬛⭐⭐⭐⭐⬛
+⭐⬛⬛⭐⬛⭐⬛⬛⬛⬛⭐⬛⭐⬛⬛⬛⬛⬛⭐⬛⭐⬛⬛⭐⬛⭐⬛⬛⭐⬛⭐⬛⬛⬛⬛⭐⬛⬛⬛⬛
+⭐⬛⬛⭐⬛⭐⭐⭐⬛⬛⭐⭐⬛⬛⬛⬛⬛⭐⬛⬛⭐⬛⬛⬛⬛⭐⬛⬛⭐⬛⭐⭐⭐⬛⬛⭐⭐⭐⬛⬛
+⭐⭐⭐⬛⬛⭐⬛⬛⬛⬛⭐⬛⭐⬛⬛⬛⭐⬛⬛⬛⭐⬛⬛⬛⬛⭐⭐⭐⬛⬛⭐⬛⬛⬛⬛⭐⬛⬛⬛⬛
+⭐⬛⭐⬛⬛⭐⬛⬛⬛⬛⭐⬛⭐⬛⬛⭐⬛⬛⬛⬛⭐⬛⬛⭐⬛⭐⬛⬛⬛⬛⭐⬛⬛⬛⬛⭐⬛⬛⬛⬛
+⭐⬛⬛⭐⬛⭐⬛⬛⬛⬛⭐⬛⬛⭐⬛⭐⭐⭐⭐⬛⬛⭐⭐⬛⬛⭐⬛⬛⬛⬛⭐⭐⭐⭐⬛⭐⬛⬛⬛⬛
+"
